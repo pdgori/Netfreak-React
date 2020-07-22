97faa26f0edd1d9fbc874dd4943e972723f57dba
Phillipe Gori <pdgori@hotmail.com>
2020-07-22 22:17:13 +0100

    netflix clone (netfreak) primeiro push.

---
+++ src/components/Nav.js
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from "react";
+import "./Nav.css";
+function Nav() {
+  const [handleShow, setHandleShow] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        setHandleShow(true);
+      } else {
+        setHandleShow(false);
+      }
+    });
+
+    return () => {
+      window.removeEventListener("scroll");
+    };
+  }, []);
+
+  return (
+    <div className={`nav ${handleShow && "nav__black"}`}>
+      <img
+        className="nav__logo"
+        src="https://upload.wikimedia.org/wikipedia/commons/0/0f/Logo_Netflix.png"
+        alt="Netflix Logo"
+      />
+      <img
+        className="nav__avatar"
+        src="https://pbs.twimg.com/profile_images/1240119990411550720/hBEe3tdn_400x400.png"
+        alt="Netflix Logo"
+      />
+    </div>
+  );
+}
+
+export default Nav;
